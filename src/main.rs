fe62b9da9fad0468d26b43ab7c5ddf24d584f5d6
Takeshi Sekiya <takeshi.sekiya@woven-planet.global>
2022-08-05 16:27:00 +0900

    add log

--- src/main.rs
+++ src/main.rs
@@ -323,6 +323,7 @@ async fn get_watchs() -> Result<HashSet<String>, Box<dyn std::error::Error>> {
     let mut set = HashSet::<String>::new();
     let mut start = 1;
     loop {
+        println!("get_watchs: start = {}", start);
         let url = env::var("WATCH_URL")?.to_owned() + &start.to_string();
         let body = reqwest::get(url).await?.json::<Watches>().await?;
         let re = Regex::new(r"www\.youtube\.com/watch\?v=(.{11})").unwrap();
